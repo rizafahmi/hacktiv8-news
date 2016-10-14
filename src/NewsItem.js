812fed0f142849d5912b1f71c9a22e9540c53c6d
Riza Fahmi <rizafahmi@gmail.com>
2016-10-14 13:18:51 +0700

    Inline styling

--- src/NewsItem.js
+++ src/NewsItem.js
@@ -2,13 +2,16 @@ import React, {Component} from 'react'
 
 class NewsItem extends Component {
   render() {
+    let metaStyle = {
+      color: 'grey'
+    }
     return (
       <div>
         <div className="news-item">
           <div className="title">
           1. Mengapa Memilih Coding Bootcamp? <span className="domain">(hacktiv8.com/blog)</span>
           </div>
-          <div className="meta">
+          <div className="meta" style={metaStyle}>
             214 points by <span className="by">rizafahmi</span> 
             <span className="date">5 hours ago</span> |
             <span className="comment-count"> 54 comments</span>
