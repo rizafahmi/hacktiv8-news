14ebf9bff6d93075811145ec74b2cd0b24fe1574
Riza Fahmi <rizafahmi@gmail.com>
2016-10-14 12:58:17 +0700

    Add NewsHeader and NewsItem components

---
+++ src/NewsItem.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        <div className="news-item">Sit odit illo numquam labore soluta?</div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
